4755511d7f852c9606a9bd6a2ed307d9e5b5892a
Alex <flotcka@gmail.com>
2020-06-24 17:40:31 +0300

    secondBranch

--- src/test/java/TestClassForGit.java
+++ src/test/java/TestClassForGit.java
@@ -2,7 +2,7 @@ import org.testng.annotations.Test;
 
 public class TestClassForGit {
     @Test
-    public void testMethod() {
+    public void testMethod2() {
         System.out.println("second testBranch");
     }
 
