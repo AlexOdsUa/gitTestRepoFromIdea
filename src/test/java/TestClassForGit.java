e63b874cce13e956bf82ddf3f3103d6f736bbb67
Alex <flotcka@gmail.com>
2020-06-24 17:36:40 +0300

    secondBranch

--- src/test/java/TestClassForGit.java
+++ src/test/java/TestClassForGit.java
@@ -1,36 +1,9 @@
 import org.testng.annotations.Test;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class TestClassForGit {
     @Test
     public void testMethod() {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        Date date = new Date();
-        System.out.println(formatter.format(date));
+        System.out.println("second testBranch");
     }
-
-    @Test
-    public void testMethod2() {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy 'HAHAHA' HH:mm:ss");
-        Date date = new Date();
-        System.out.println(formatter.format(date));
-    }
-
-    @Test
-    public void testMethod3() {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy 'HelloWorld' HH:mm:ss");
-        Date date = new Date();
-        System.out.println(formatter.format(date));
-    }
-    
-        @Test
-    public void testMethod4() {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy 'HelloWorld' HH:mm:ss");
-        Date date = new Date();
-        System.out.println(formatter.format(date));
-    }
-
 
 }
