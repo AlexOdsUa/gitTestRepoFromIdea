dfd877baf7681b90948f625ca25b06dae5acd30b
Alex <flotcka@gmail.com>
2020-06-24 17:42:56 +0300

    totalCommit

--- src/test/java/TestClassForGit.java
+++ src/test/java/TestClassForGit.java
@@ -1,15 +1,10 @@
 import org.testng.annotations.Test;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class TestClassForGit {
     @Test
     public void testMethod() {
         System.out.println("master branch");
     }
-
-
 
     @Test
     public void testMethod2() {
