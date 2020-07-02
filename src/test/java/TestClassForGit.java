264e6d01709fb11eda6da86a09dca7bec3741335
Alex <flotcka@gmail.com>
2020-07-02 10:40:31 +0300

    8

--- src/test/java/TestClassForGit.java
+++ src/test/java/TestClassForGit.java
@@ -3,34 +3,42 @@ import org.testng.annotations.Test;
 public class TestClassForGit {
     @Test
     public void testMethod() {
-        System.out.println("master branch");
+        System.out.println("testMethod");
     }
 
     @Test
     public void testMethod2() {
-        System.out.println("second testBranch");
+        System.out.println("testMethod2");
     }
 
     @Test
     public void testMethod3() {
-        System.out.println("third testBranch");
+        System.out.println("testMethod3");
     }
-
 
     @Test
     public void testMethod4() {
-        System.out.println("finalMerge2");
+        System.out.println("testMethod4");
     }
-    
-        @Test
+
+    @Test
+    public void testMethod5() {
+        System.out.println("testMethod5");
+    }
+
+    @Test
     public void testMethod6() {
-        System.out.println("changeFromGit");
+        System.out.println("testMethod6");
     }
-    
-        
-        @Test
+
+    @Test
+    public void testMethod7() {
+        System.out.println("testMethod7");
+    }
+
+    @Test
     public void testMethod8() {
-        System.out.println("changeFromGit8");
+        System.out.println("testMethod8");
     }
 
 
